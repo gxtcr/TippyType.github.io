c31360a3dea300f236a04a387e5dfc3aaac57fdc
gxtcr <gwentech@hotmail.fr>
2025-03-30 19:07:40 +0900

    New config file

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,10 @@
 //Words lists used for the game
 const listeMots = ["Cinnamonroll", "Pétunia", "Serviette", "Chocolat"]
+const listeMots2 = ["Minecraft", "Tetris", "Overcooked", "ZigZagRacer", "TheSims4"]
+
+//Faire un random i entre 0 et le listeMotsGlobale.length et dire que ta liste actuelle de jeu est la liste de  listeMotsGlobale[i]
+const listeMotsGlobale = [listeMots, listeMots2]
+
 const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson", "Je veux du chocolat ou je pique une crise"]
+
+
